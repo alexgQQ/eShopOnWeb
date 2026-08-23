2f976e7309026740426f1e9edd646f7147d14fee
Alex Grand <me@alexgrand.dev>
2026-08-22 20:44:41 -0600

    test commit

--- src/Web/Controllers/ManageController.cs
+++ src/Web/Controllers/ManageController.cs
@@ -343,13 +343,13 @@ public class ManageController : Controller
     [HttpGet]
     public IActionResult ShowRecoveryCodes()
     {
-        var recoveryCodes = (string[]?)TempData[RecoveryCodesKey];
-        if (recoveryCodes == null)
+        var _recoveryCodes = (string[]?)TempData[RecoveryCodesKey];
+        if (_recoveryCodes == null)
         {
             return RedirectToAction(nameof(TwoFactorAuthentication));
         }
 
-        var model = new ShowRecoveryCodesViewModel { RecoveryCodes = recoveryCodes };
+        var model = new ShowRecoveryCodesViewModel { RecoveryCodes = _recoveryCodes };
         return View(model);
     }
 
